16914fe7372527632f281bad615ff59f0c6b4fb0
betterer <54624517+better-er@users.noreply.github.com>
2026-01-11 19:49:39 +0800

    增加加载中提示，使用jpg质量50压缩加速，噼里啪啦图床403

--- assets/js/mygica.js
+++ assets/js/mygica.js
@@ -1,48 +1,48 @@
 const imageMap = {
     MyGO: [
-        'MyGICA-fast/MyGO/mygo_changes_01.jpg',
-        'MyGICA-fast/MyGO/mygo_changes_02.jpg',
-        'MyGICA-fast/MyGO/mygo_changes_03.jpg',
-        'MyGICA-fast/MyGO/mygo_changes_04.jpg',
-        'MyGICA-fast/MyGO/mygo_changes_05.jpg',
-        'MyGICA-fast/MyGO/mygo_changes_06.jpg',
-        'MyGICA-fast/MyGO/mygo_changes_07.jpg',
-        'MyGICA-fast/MyGO/mygo_changes_08.jpg',
-        'MyGICA-fast/MyGO/mygo_changes_09.jpg',
-        'MyGICA-fast/MyGO/mygo_changes_10.jpg',
-        'MyGICA-fast/MyGO/mygo_changes_11.jpg',
-        'MyGICA-fast/MyGO/mygo_changes_12.jpg',
-        'MyGICA-fast/MyGO/mygo_changes_13.jpg',
+        ['https://i0.hdslb.com/bfs/openplatform/095dd28c2f9873cefa4ea0998e26f4093f6e1024.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/MyGO/mygo_changes_01.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/aabbc5a52e63d589c126a8e18c9f9392382ad57e.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/MyGO/mygo_changes_02.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/eccc62a902a33da3ff6bbfd192440dd39fb598da.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/MyGO/mygo_changes_03.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/b65706ad519526203459c1a87d963808d6b150bb.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/MyGO/mygo_changes_04.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/5ad971b68a5894df19122da6960520e0a488f3bc.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/MyGO/mygo_changes_05.jpg'],
+        ['https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/MyGO/mygo_changes_06.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/d130cf5daa0d3928cb4b517aa8f11cb46de9b30c.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/MyGO/mygo_changes_07.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/1746a7628ebe7d5921fafc8283141abe23b36084.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/MyGO/mygo_changes_08.jpg'],
+        ['https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/MyGO/mygo_changes_09.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/a787d601921e9f36375ee3ed0530066e5af30b9c.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/MyGO/mygo_changes_10.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/7b1cb1d1b8b29c90abd1a5181ce7fa1aec1ae4cc.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/MyGO/mygo_changes_11.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/e052531f4b7779ab93e9cadbf556db79442340db.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/MyGO/mygo_changes_12.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/21238738d8d420c5a844b978208d03990017ec08.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/MyGO/mygo_changes_13.jpg'],
     ],
     Mujica: [
-        'MyGICA-fast/Mujica/mujica_changes_01.jpg',
-        'MyGICA-fast/Mujica/mujica_changes_02.jpg',
-        'MyGICA-fast/Mujica/mujica_changes_03.jpg',
-        'MyGICA-fast/Mujica/mujica_changes_04.jpg',
-        'MyGICA-fast/Mujica/mujica_changes_05.jpg',
-        'MyGICA-fast/Mujica/mujica_changes_06.jpg',
-        'MyGICA-fast/Mujica/mujica_changes_07.jpg',
-        'MyGICA-fast/Mujica/mujica_changes_08.jpg',
-        'MyGICA-fast/Mujica/mujica_changes_09.jpg',
-        'MyGICA-fast/Mujica/mujica_changes_10.jpg',
-        'MyGICA-fast/Mujica/mujica_changes_11.jpg',
-        'MyGICA-fast/Mujica/mujica_changes_12.jpg',
-        'MyGICA-fast/Mujica/mujica_changes_13.jpg',
+        ['https://i0.hdslb.com/bfs/openplatform/095dd28c2f9873cefa4ea0998e26f4093f6e1024.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/Mujica/mujica_changes_01.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/aabbc5a52e63d589c126a8e18c9f9392382ad57e.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/Mujica/mujica_changes_02.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/eccc62a902a33da3ff6bbfd192440dd39fb598da.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/Mujica/mujica_changes_03.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/b65706ad519526203459c1a87d963808d6b150bb.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/Mujica/mujica_changes_04.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/5ad971b68a5894df19122da6960520e0a488f3bc.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/Mujica/mujica_changes_05.jpg'],
+        ['https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/Mujica/mujica_changes_06.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/d130cf5daa0d3928cb4b517aa8f11cb46de9b30c.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/Mujica/mujica_changes_07.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/1746a7628ebe7d5921fafc8283141abe23b36084.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/Mujica/mujica_changes_08.jpg'],
+        ['https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/Mujica/mujica_changes_09.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/a787d601921e9f36375ee3ed0530066e5af30b9c.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/Mujica/mujica_changes_10.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/7b1cb1d1b8b29c90abd1a5181ce7fa1aec1ae4cc.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/Mujica/mujica_changes_11.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/e052531f4b7779ab93e9cadbf556db79442340db.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/Mujica/mujica_changes_12.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/21238738d8d420c5a844b978208d03990017ec08.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/Mujica/mujica_changes_13.jpg'],
     ],
     LycoReco: [
-        'MyGICA-fast/LycoReco/Lycoris Recoil_changes_01.jpg',
-        'MyGICA-fast/LycoReco/Lycoris Recoil_changes_02.jpg',
-        'MyGICA-fast/LycoReco/Lycoris Recoil_changes_03.jpg',
-        'MyGICA-fast/LycoReco/Lycoris Recoil_changes_04.jpg',
-        'MyGICA-fast/LycoReco/Lycoris Recoil_changes_05.jpg',
-        'MyGICA-fast/LycoReco/Lycoris Recoil_changes_06.jpg',
-        'MyGICA-fast/LycoReco/Lycoris Recoil_changes_07.jpg',
-        'MyGICA-fast/LycoReco/Lycoris Recoil_changes_08.jpg',
-        'MyGICA-fast/LycoReco/Lycoris Recoil_changes_09.jpg',
-        'MyGICA-fast/LycoReco/Lycoris Recoil_changes_10.jpg',
-        'MyGICA-fast/LycoReco/Lycoris Recoil_changes_11.jpg',
-        'MyGICA-fast/LycoReco/Lycoris Recoil_changes_12.jpg',
-        'MyGICA-fast/LycoReco/Lycoris Recoil_changes_13.jpg',
+        ['https://i0.hdslb.com/bfs/openplatform/d8c244e56eb0dcdd5cad27ec40601febc1d9b5e4.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/LycoReco/Lycoris Recoil_changes_01.jpg'],
+        ['https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/LycoReco/Lycoris Recoil_changes_02.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/e5ed7705ec8d080d7f98068135d57e91c18b9cfc.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/LycoReco/Lycoris Recoil_changes_03.jpg'],
+        ['https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/LycoReco/Lycoris Recoil_changes_04.jpg'],
+        ['https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/LycoReco/Lycoris Recoil_changes_05.jpg'],
+        ['https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/LycoReco/Lycoris Recoil_changes_06.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/1823be6e3c101ef602c7ad75f1675ffe12b29ebd.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/LycoReco/Lycoris Recoil_changes_07.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/76e538b219945a8e1785abc090c1c816fa7abf9c.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/LycoReco/Lycoris Recoil_changes_08.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/cc3a7f51e4bf99b3bcbeb931316e0206731d2e58.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/LycoReco/Lycoris Recoil_changes_09.jpg'],
+        ['https://i0.hdslb.com/bfs/openplatform/65bbf8221092bca8e843180d95021ac07a990e9b.jpg', 'https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/LycoReco/Lycoris Recoil_changes_10.jpg'],
+        ['https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/LycoReco/Lycoris Recoil_changes_11.jpg'],
+        ['https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/LycoReco/Lycoris Recoil_changes_12.jpg'],
+        ['https://raw.githubusercontent.com/better-er/MyGICA-images-storage/refs/heads/master/MyGICA-fast-small-50/LycoReco/Lycoris Recoil_changes_13.jpg'],
     ]
 };
 
@@ -52,6 +52,32 @@ const gallery = document.getElementById('gallery');
 const imgModal = document.getElementById('imgModal');
 const modalImg = document.getElementById('modalImg');
 const modalClose = document.getElementById('modalClose');
+
+function tryLoadSequential(imgEl, srcList, onSuccess, onFailure) {
+    // srcList: URI（字符串）数组。按顺序尝试每个，直到加载成功为止。
+    const filtered = (srcList || []).map(s => (typeof s === 'string' ? s.trim() : '')).filter(Boolean);
+    let idx = 0;
+    function attempt() {
+        if (idx >= filtered.length) {
+            if (onFailure) onFailure();
+            return;
+        }
+        const s = filtered[idx++];
+        // 在设置src之前分配处理程序
+        imgEl.onerror = () => {
+            // 尝试下一个
+            attempt();
+        };
+        imgEl.onload = () => {
+            // 成功
+            imgEl.onerror = null;
+            if (onSuccess) onSuccess(s);
+        };
+        // 触发加载
+        imgEl.src = s;
+    }
+    attempt();
+}
 
 function renderGallery() {
     const folder = folderSelect.value;
@@ -66,7 +92,10 @@ function renderGallery() {
         gallery.style.marginLeft = 'calc(-1 * ((100vw - 100%) / 2))';
         gallery.style.marginRight = '0';
 
-        images.forEach((src, idx) => {
+        images.forEach((srcs, idx) => {
+            // 规范化为数组
+            if (!Array.isArray(srcs)) srcs = [srcs];
+
             const item = document.createElement('div');
             item.className = 'gallery-item';
 
@@ -76,9 +105,33 @@ function renderGallery() {
             item.appendChild(num);
 
             const img = document.createElement('img');
-            img.src = src;
-            img.alt = src.split('/').pop();
+            img.alt = (Array.isArray(srcs) ? srcs[0] : String(srcs)).split('/').pop();
+            img.style.transition = 'all 0.4s ease';
             item.appendChild(img);
+
+            // 尝试顺序加载；成功后存储成功的URL
+            tryLoadSequential(img, srcs, (successfulUrl) => {
+                img.dataset.loadedSrc = successfulUrl;
+                img.style.filter = 'none';
+                img.style.background = 'transparent';
+            }, () => {
+                // 全部失败
+                const failTip = document.createElement('div');
+                failTip.className = 'thumb-tip';
+                failTip.textContent = '图片加载失败';
+                item.style.position = 'relative';
+                item.appendChild(failTip);
+            });
+
+            // 点击时如果已加载则打开模态框
+            item.onclick = () => {
+                const src = img.dataset.loadedSrc || img.src;
+                if (src) {
+                    modalImg.src = src;
+                    imgModal.classList.add('active');
+                }
+            };
+
             gallery.appendChild(item);
         });
     } else {
@@ -86,13 +139,15 @@ function renderGallery() {
         gallery.style.marginLeft = '';
         gallery.style.marginRight = '';
 
-        images.forEach(src => {
+        images.forEach(srcs => {
+            if (!Array.isArray(srcs)) srcs = [srcs];
+
             const item = document.createElement('div');
             item.className = 'gallery-item';
 
             const img = document.createElement('img');
-            img.alt = src.split('/').pop();
-            img.setAttribute('data-src', src);
+            img.alt = srcs[0].split('/').pop();
+            img.setAttribute('data-srcs', JSON.stringify(srcs));
             img.src = '';
             img.style.filter = 'blur(8px)';
             img.style.background = '#f5f5f5';
@@ -107,18 +162,40 @@ function renderGallery() {
 
             let loaded = false;
             item.onclick = () => {
+                // 防止在加载时重复尝试
+                if (item.dataset.loading) return;
+
                 if (!loaded) {
-                    img.src = src;
-                    img.onload = () => {
+                    // 标记为正在加载
+                    item.dataset.loading = '1';
+
+                    // 立即显示加载状态
+                    tip.textContent = '加载中...';
+
+                    const list = (JSON.parse(img.getAttribute('data-srcs') || '[]') || []).map(s => (typeof s === 'string' ? s.trim() : '')).filter(Boolean);
+
+                    tryLoadSequential(img, list, (successfulUrl) => {
+                        // 成功时立即移除遮罩/模糊和提示
                         img.style.filter = 'none';
                         img.style.background = 'transparent';
-                        tip.style.opacity = '0';
-                        setTimeout(() => tip.remove(), 300);
+                        if (tip && tip.parentNode) tip.parentNode.removeChild(tip);
+
                         loaded = true;
-                    };
+                        img.dataset.loadedSrc = successfulUrl;
+
+                        // 清除加载标志
+                        delete item.dataset.loading;
+                    }, () => {
+                        tip.textContent = '图片加载失败';
+                        // 清除加载标志
+                        delete item.dataset.loading;
+                    });
                 } else {
-                    modalImg.src = src;
-                    imgModal.classList.add('active');
+                    const src = img.dataset.loadedSrc || img.src;
+                    if (src) {
+                        modalImg.src = src;
+                        imgModal.classList.add('active');
+                    }
                 }
             };
 
